df73f973ef599bb7f4526baf9b9b5eece84dc124
ihnic <nicholas_tzw@hotmail.com>
2025-02-07 10:19:47 +0800

    udpate mihomo

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -12,7 +12,7 @@ sed -i "/luci-app-cpufreq/d" include/target.mk
 sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
 
 # Not generate ISO images for it is too big
-sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
+#sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
 
 # Not generate VHDX images
 sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
